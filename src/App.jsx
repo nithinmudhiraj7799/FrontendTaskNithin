7d7ac7e04b28ef9b85ee9eb3b003115bede12d6e
nithinmudhiraj7799 <talarinithin7799@gmail.com>
2025-05-24 19:01:54 +0530

    routing corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
+import { Routes, Route } from 'react-router-dom'; 
+import Home from './components/home';
+import Login from './components/login';
+import Signup from './components/signup';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
